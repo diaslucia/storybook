b6669f344b2bb829c92a69ff3a182478aae18c85
Lucia Dias <88150989+diaslucia@users.noreply.github.com>
2024-04-18 18:12:49 -0300

    Delete dist directory

--- dist/esm/types/src/stories/buttons/Button.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Meta, StoryObj } from "@storybook/react";
-declare const meta: Meta;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const Primary: Story;
-export declare const Secondary: Story;
-export declare const Grey: Story;
-export declare const IconButton: Story;
--- dist/esm/types/src/stories/buttons/IconButton.d.ts
+++
@@ -1,5 +0,0 @@
-interface Props {
-    icon: string;
-}
-declare const IconButton: ({ icon }: Props) => import("react/jsx-runtime").JSX.Element;
-export default IconButton;
