11302896143f6618d849207bb3b7c7c98ae6c394
diaslucia <luciadias.dev@gmail.com>
2024-04-18 13:59:29 -0300

    try

--- src/stories/buttons/Button.stories.ts
+++ src/stories/buttons/Button.stories.ts
@@ -1,5 +1,5 @@
+import Button from "@/stories/buttons";
 import type { Meta, StoryObj } from "@storybook/react";
-import Button from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
